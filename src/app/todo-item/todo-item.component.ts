90dff73be31dd0b74b9f34bf327c9926e5b8fe63
Pierre Blarre <pierre.blarre@gmail.com>
2019-11-14 10:29:46 +0100

    Working V1

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,16 +1,50 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { TodoService } from "../todo.service";
+import { TodoItemData } from "../dataTypes/TodoItemData";
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() private data: TodoItemData;
+ 
+  @ViewChild("newTextInput", {static: false}) private inputLabel: ElementRef;
 
-  ngOnInit() {
+  private _editionMode = false;
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {}
+
+  get editionMode(): boolean {
+    return this._editionMode;
   }
 
+  set editionMode(e: boolean) {
+    this._editionMode = e;
+    requestAnimationFrame(() => this.inputLabel.nativeElement.focus());
+  }
+
+  get label(): string {
+    return this.data.label;
+  }
+
+  set label(lab: string) {
+    this.todoService.setItemsLabel(lab, this.data);
+  }
+
+  get isDone(): boolean {
+    return this.data.isDone;
+  }
+
+  set isDone(done: boolean) {
+    this.todoService.setItemsDone(done, this.data);
+  }
+
+  destroy() {
+    this.todoService.removeItems(this.data);
+  }
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,32 +1,65 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild, ElementRef, HostListener} from '@angular/core';
 import {TodoListData} from '../dataTypes/TodoListData';
 import {TodoItemData} from '../dataTypes/TodoItemData';
 import {TodoService} from '../todo.service';
 
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  
+  titre: string;
 
-  @Input() 
-  
-  private data: TodoListData;
-  
-  constructor(private todoService: TodoService) { 
+  @Input() private data: TodoListData;
+
+  @ViewChild("newTodoInput", {static: false}) newTodoInput: ElementRef;
+
+  constructor(private todoService: TodoService) {  
+    todoService.getTodoListDataObserver().subscribe( tdl => this.data = tdl );  
+    this.titre = this.data.label;
   }
 
   ngOnInit() {
   }
 
   get label(): string {
-    return this.data ? this.data.label : '';
+    return this.data.label;
   }
-
+  
   get items(): TodoItemData[] {
-    return this.data ? this.data.items : [];
+    return this.data.items;
+  }
+  
+  itemDone(item: TodoItemData, done: boolean) {
+    this.todoService.setItemsDone( done, item );
+  }
+  
+  itemLabel(item: TodoItemData, label: string) {
+    this.todoService.setItemsLabel( label, item );
+  }
+  
+  appendItem(label: string) {
+    this.todoService.appendItems( {
+      label,
+      isDone: false
+    } );
+  }
+  
+  removeItem(item: TodoItemData) {
+    this.todoService.removeItems(item);
+  }
+    
+  isAllDone(): boolean {
+    // return this.items.reduce( (acc, it) => acc && it.isDone, true);
+    return this.items.every( it => it.isDone );
+  }
+  
+  toggleAllDone() {
+    const done = !this.isAllDone();
+    this.todoService.setItemsDone(done, ...this.items);
   }
 
 }
